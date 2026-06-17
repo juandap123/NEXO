7de72a7de81bc48f81e1aa0427a0d5b49c05ec2b
juandap123 <juandavidcardozo374@gmail.com>
2026-06-17 15:01:59 -0500

    menu

--- nexo/src/componente/Menu.jsx
+++ nexo/src/componente/Menu.jsx
@@ -6,21 +6,20 @@ function Menu() {
         <div className="menu">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <Link class="navbar-brand" to="/    ">Nexo</Link>
+                    <Link class="navbar-brand" to="/">Nexo</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <Link class="nav-link active" aria-current="page" to="/">Home</a>
+                                <Link class="nav-link active" aria-current="page" to="/contacto">Contacto</Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/">Link</a>
+                                <Link class="nav-link" to="/registro">Registro</Link>
                             </li>
-                            
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <Link class="nav-link disabled" aria-disabled="true" to="/inicio">Inicio</Link>
                             </li>
                         </ul>
                         <form class="d-flex" role="search">
